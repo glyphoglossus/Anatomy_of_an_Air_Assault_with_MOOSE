b4577724799cf0382e936bc007347478d8a631cb
Glyphoglossus <glyphoglossus@gmail.com>
2016-05-03 23:35:49 -0400

    Initial commit

---
+++ air_assault_moose.lua
@@ -0,0 +1,9 @@
+
+Croc1_Spawner = SPAWN:New("Croc1_Template")
+Croc1 = Croc1_Spawner:Spawn()
+
+-- Gator_Spawner = SPAWN:New("Gator_Template")
+-- Gator1 = Gator_Spawner:Spawn()
+-- Gator2 = Gator_Spawner:Spawn()
+-- Gator3 = Gator_Spawner:Spawn()
+
